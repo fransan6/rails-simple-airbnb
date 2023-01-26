2e491150049de69cf5cc1e2a6f9e68856dd2cdf5
francesca sandford <fran6san@gmail.com>
2023-01-26 21:32:31 +0000

    finished main challenge

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,27 +1,57 @@
 class FlatsController < ApplicationController
-  # before_action :set_flat, only: %i[index show]
+  before_action :set_flat, only: %i[show edit update destroy]
 
   def index
     @flats = Flat.all
+
+    if params[:query].present?
+      @query = params[:query]
+      @flats = Flat.where("name LIKE ?", "%#{params[:query]}%")
+      raise
+    else
+      @flats = Flat.all
+    end
   end
 
-  def show
-    @flat = Flat.find(params[:id])
+  # hidden_field_tag
+
+  def show; end
+  def edit; end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.create(flat_params)
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @flat.update(flat_params)
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flat_path, status: :see_other
   end
 
   private
 
-  # def set_flat
-  #   @flat = Flat.find(params[:id])
-  # end
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
 
   def flat_params
-    params.requre(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
   end
 end
-
-# t.string "name"
-# t.string "address"
-# t.text "description"
-# t.integer "price_per_night"
-# t.integer "number_of_guests"
